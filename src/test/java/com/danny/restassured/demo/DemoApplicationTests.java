4186e718ec18005b8dc45f64ba201746fb01a95d
Danielly Marques <danielly.marques@dbserver.com.br>
2025-05-06 12:04:12 -0300

    feature/estruturacao-projeto

---
+++ src/test/java/com/danny/restassured/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.restassured.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
